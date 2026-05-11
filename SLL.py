e6eba93a4b27a50ec97ad3a5f2beefa47de9426d
Ayushi <73694291+Ayushi2006@users.noreply.github.com>
2026-05-11 19:22:40 +0530

    Update SLL.py

--- SLL.py
+++ SLL.py
@@ -1,7 +1,7 @@
 # not efficient improvement needed
 class Node:
  
-    def __init__(self, item=None, next=None):  
+    def __init__(self, item=None, next=None):   
         self.item=item    
         self.next=next 
   
